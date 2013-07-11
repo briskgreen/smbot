1caec56004a709d112ff3af992429b76c6ed7a6a
briskgreen <briskgreen@163.com>
2013-07-12 03:09:06 +0800

    updata

---
+++ mysock/mysock.h
@@ -0,0 +1,42 @@
+#ifndef _MYSOCK_H
+#define _MYSOCK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define MEM_SIZE 80
+
+typedef struct sockaddr SA;
+typedef struct sockaddr_in SA_IN;
+
+void error_quit(const char *msg);
+
+int Socket(int domain,int type,int prot);
+
+void Bind(int sockfd,SA_IN *server_addr);
+
+void Listen(int sockfd,int backlog);
+
+int Accept(int sockfd,SA_IN *client_addr,socklen_t *size);
+
+void Connect(int sockfd,SA_IN *server_addr);
+
+void init_data_with_server(SA_IN *server_addr,int port);
+
+void init_data_with_client(SA_IN *server_addr,char *host,int port);
+
+char *read_line(int sockfd);
+
+int tcp_conect(char *url,int port);
+
+char *url_encode(char *string);
+
+#endif
