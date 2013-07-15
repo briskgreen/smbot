86e897638e6a69f22fc7f0af229ce9d7542fefac
briskgreen <briskgreen@163.com>
2013-07-16 01:44:42 +0800

    updata

--- bt.c
+++ bt.c
@@ -7,6 +7,7 @@ char *get_bt_magnet(char *buf)
 	int pipefd[2];
 
 	pipe(pipefd);
+	temp=url_encode(buf);
 
 	if(fork()==0)
 	{
@@ -15,10 +16,11 @@ char *get_bt_magnet(char *buf)
 		dup2(pipefd[1],STDOUT_FILENO);
 		dup2(pipefd[1],STDERR_FILENO);
 
-		execl("sh/bt.sh","bt",buf,NULL);
+		execl("sh/bt.sh","bt",temp,NULL);
 	}
 
 	close(pipefd[1]);
+	safe_free(&temp);
 
 	read(pipefd[0],res,sizeof(res));
 	temp=malloc(strlen(res)+2);
