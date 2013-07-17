6c44e672039d108a4d67c31622a5630c3d530da9
briskgreen <briskgreen@163.com>
2013-07-18 03:45:48 +0800

    updata

--- sh/bing.sh
+++ sh/bing.sh
@@ -2,6 +2,6 @@
 
 url=`exec/url_encode $1`
 
-exec/regex \<div\ class=\"sb_tlst\ sbt_ad\"\>.*href=\".*target=\"_blank\".*\<cite\> "`curl -s "http://cn.bing.com/search?q=$url"`" | awk -F"(<cite>)" '{print $1}' | awk -F"(</p>)" '{print $1}' | sed 's/<div class="sb_tlst sbt_ad">.*<a href="//g' | sed 's/" target="_blank".*<p>/ <--/g' | sed 's/<strong>//g' | sed 's/<\/strong>//g'
+exec/regex \<div\ class=\"sb_tlst\ sbt_ad\"\>.*href=\".*target=\"_blank\".*\<cite\> "`curl -s "http://cn.bing.com/search?q=$url"`" | awk -F"(<cite>)" '{print $1}' | awk -F"(</p>)" '{print $1}' | sed 's/<div class="sb_tlst sbt_ad">.[^<]*<a href="//g' | sed 's/" target="_blank".*<p>/ <--/g' | sed 's/<strong>//g' | sed 's/<\/strong>//g' | sed 's/<div class="sb_tlst sbt_ad"><a href="//g' | sed 's/" class="sb_fav//g'
 
 exit 0
