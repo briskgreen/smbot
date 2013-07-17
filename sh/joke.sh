ce1facf3985666fccbba3dfe1ade7f4b8530209e
briskgreen <briskgreen@163.com>
2013-07-17 21:57:46 +0800

    updata

--- sh/joke.sh
+++ sh/joke.sh
@@ -2,6 +2,6 @@
 
 url=`exec/url_encode $1`
 
-exec/regex \<p\>.[^\<]* "`curl -s "http://www.fangtang8.com/?s=$url"`" | tr -d "<p>"
+exec/regex \<p\>.[^\<]* "`curl -s "http://www.fangtang8.com/?s=$url"`" | sed 's/<p>//g'
 
 exit 0
