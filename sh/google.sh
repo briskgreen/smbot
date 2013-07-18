4db7dab21a884f6f844fb5cebf55ba4e80650c15
briskgreen <briskgreen@163.com>
2013-07-19 03:25:21 +0800

    updata

--- sh/google.sh
+++ sh/google.sh
@@ -2,7 +2,7 @@
 
 in=`curl -s "https://www.googleapis.com/customsearch/v1?key=yourkey&cx=006431901905483214390:i3yxhoqkzo0&num=1&alt=atom&q=$1"`
 url=`exec/regex href=\".[^\"]* "$in" | sed 's/href="//g'`
-des=`exec/regex \<summary.*\<\/summary\> "$in" | sed 's/<sum.[^;]*//g' | sed 's/.b&.[^;]*//g' | sed 's/&.[^;]*//g' | sed 's/;//g' | sed 's/<\/summary>//g' | sed 's/br//g' | sed 's/nbsp//g'`
+des=`exec/regex \<summary.*\<\/summary\> "$in" | sed 's/<sum.[^;]*//g' | sed 's/.b&.[^;]*//g' | sed 's/&.[^;]*//g' | sed 's/;//g' | sed 's/<\/summary>//g' | sed 's/br//g' | sed 's/nbsp//g' | sed 's/>.*days ago ...//g'`
 
 echo "$url <--$des"
 
