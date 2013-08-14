9a51199e0c3571cc8ba36c0cc7523996aff3bf3d
briskgreen <briskgreen@163.com>
2013-08-14 22:53:48 +0800

    updata

--- new/smbot/command.c
+++ new/smbot/command.c
@@ -44,6 +44,86 @@ void get_man_url(SMBOT_DATA *data)
 	smbot_destory(data);
 }
 
+void get_ip_addr(SMBOT_DATA *data)
+{
+	pid_t pid;
+	int pipefd[2];
+	char temp[1024]={0};
+
+	pipe(pipefd);
+	if((pid=fork()) == 0)
+	{
+		close(pipefd[0]);
+		dup2(STDOUT_FILENO,pipefd[1]);
+		dup2(STDERR_FILENO,pipefd[1]);
+
+		execl("exec/query_ip_addr","query_ip_addr",data->arg,NULL);
+	}
+
+	close(pipefd[1]);
+	read(pipefd[0],temp,sizeof(temp));
+	waitpid(pid,NULL,WNOHANG);
+
+	msg_send(temp,data);
+	smbot_destory(data);
+}
+
+void bing_dict(SMBOT_DATA *data)
+{
+	char *res;
+	char *buf;
+	HTTP *http;
+	int i;
+
+	for(i=0;data->arg[i]!= ' ';++i);
+	if(data->arg[i] == '\0')
+	{
+		msg_send("错误的格式",data);
+		smbot_destory(data);
+		return;
+	}
+	buf=strnstr(data->arg,i);
+
+	http=http_head_init();
+	res=string_add("GET /Bing/MicrosoftTranslator/v1/Translate?Text=%27%s%27&To=%27%s%27&$skip=0&$top=1&$format=json HTTP/1.1\n",data->arg+i,buf);
+	null_no_free(buf);
+	http_head_add(http,res);
+	null_no_free(res);
+	http_head_add(http,"Host: api.datamarket.azure.com\n");
+	http_head_add(http,"Authorization: Basic OisrZkFPcUREQUFZYmxQeExMemVXYUE0WGJqVUdGc0FqZ0RDY1FZRFhOT0U=\n");
+	http_head_add(http,"Accept: */*\n");
+	http_head_add(http,"Connection: close\n\n");
+
+	res=https_perform(http,"api.datamarket.azure.com",443,NULL,NULL);
+	if(res == NULL)
+	{
+		msg_send("Sorry,连接到远程服务器出错",data);
+		smbot_destory(data);
+		http_head_destroy(http);
+		return;
+	}
+
+	buf=match_string("\"Text\":.[^\"]*",res);
+	null_no_free(res);
+	if(buf == NULL)
+	{
+		msg_send("Sorry no result found!",data);
+		smbot_destory(data);
+		http_head_destroy(http);
+		return;
+	}
+
+	res=strnstr(buf+8,strlen(buf)-8);
+	null_no_free(buf);
+	msg_send(res,data);
+	null_no_free(res);
+	smbot_destory(data);
+	http_head_destroy(http);
+}
+
+void get_torrent(SMBOT_DATA *data)
+{}
+
 void msg_send(const char *msg,SMBOT_DATA *data)
 {
 	if(data->is_use_ssl)
