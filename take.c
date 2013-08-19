58b66bb45cfbffb87c64ba935458cd4e364cbd6a
brisk <brisk@brisk-PC.brisk.org>
2013-08-19 18:07:36 +0800

    update

--- take.c
+++ take.c
@@ -1,5 +1,56 @@
 #include "mysock/mysock.h"
 
+unsigned int unicode_len(const char *str)
+{
+	unsigned int len;
+	int i;
+
+	for(i=0,len=0;str[i];++i,++len)
+	{
+		if(str[i] == '\\' && str[i+1] == 'u')
+			i+=5;
+	}
+
+	return len;
+}
+
+char *unicode_to_utf(const char *str)
+{
+	char *temp;
+	char *res;
+	char *buf;
+	int len;
+	int i,j;
+
+	len=unicode_len(str);
+	len*=2;
+	temp=malloc(len+1);
+	res=malloc(len*2);
+	bzero(res,len*2);
+	bzero(temp,len+1);
+
+	for(i=0,j=0;str[i];++i,++j)
+	{
+		if(str[i] == '\\' && str[i+1] == 'u')
+		{
+			buf=strnstr(str+i+4,2);
+			temp[j]=htoi(buf);
+			free(buf);
+			buf=strnstr(str+i+2,2);
+			temp[++j]=htoi(buf);
+			free(buf);
+			i+=5;
+		}
+		else
+			temp[j]=str[i];
+	}
+
+	to_iconv("UNICODE//","UTF-8//IGNORE",temp,len,res,len*2);
+	free(temp);
+
+	return res;
+}
+
 int main(int argc,char **argv)
 {
 	char *res;
@@ -16,8 +67,11 @@ int main(int argc,char **argv)
 
 	buf=match_string("test(\".[^\"]*",res);
 	free(res);
-	printf("%s",buf);
+	printf("%s\n",buf);
+	res=unicode_to_utf(buf+6);
 	free(buf);
+	printf("%s\n",res);
+	free(res);
 
 	return 0;
 }
