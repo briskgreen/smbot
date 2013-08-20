a15f7e74a8ba66953c422fab7a34f8785a5b5cce
briskgreen <briskgreen@163.com>
2013-08-21 01:55:09 +0800

    updata

--- take.c
+++ take.c
@@ -1,60 +1,10 @@
 #include "mysock/mysock.h"
 
-unsigned int unicode_len(const char *str)
-{
-	unsigned int len;
-	int i;
-
-	for(i=0,len=0;str[i];++i,++len)
-	{
-		if(str[i] == '\\' && str[i+1] == 'u')
-			i+=5;
-	}
-
-	return len;
-}
-
-char *unicode_to_utf(const char *str)
-{
-	char *temp;
-	char *res;
-	char *buf;
-	int len;
-	int i,j;
-
-	len=unicode_len(str);
-	len*=2;
-	temp=malloc(len+1);
-	res=malloc(len*2);
-	bzero(res,len*2);
-	bzero(temp,len+1);
-
-	for(i=0,j=0;str[i];++i,++j)
-	{
-		if(str[i] == '\\' && str[i+1] == 'u')
-		{
-			buf=strnstr(str+i+4,2);
-			temp[j]=htoi(buf);
-			free(buf);
-			buf=strnstr(str+i+2,2);
-			temp[++j]=htoi(buf);
-			free(buf);
-			i+=5;
-		}
-		else
-			temp[j]=str[i];
-	}
-
-	to_iconv("UNICODE//","UTF-8//IGNORE",temp,len,res,len*2);
-	free(temp);
-
-	return res;
-}
-
 int main(int argc,char **argv)
 {
 	char *res;
 	char *buf;
+	char temp[512];
 	int i;
 
 	for(i=0;argv[1][i];++i)
@@ -70,7 +20,11 @@ int main(int argc,char **argv)
 	printf("%s\n",buf);
 	res=unicode_to_utf(buf+6);
 	free(buf);
-	printf("%s\n",res);
+
+	if(strreplace(res,"<br>","",temp,511) != -1)
+		printf("%s\n",temp);
+	else
+		printf("%s\n",res);
 	free(res);
 
 	return 0;
