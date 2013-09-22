a9df9d0750bc6362358b1b786ed2e209bd5f1afd
briskgreen <briskgreen@163.com>
2013-09-22 20:28:05 +0800

    更新匹配规则

--- main.c
+++ main.c
@@ -211,10 +211,10 @@ int main(int argc,char **argv)
 		printf("%s\n",data);
 
 		if(strstr(data,"!man"))
-			parse_arg("PRIVMSG .[^ ]* :!man","!man [0-9] <要查询的内容> 功能:返回linuxmanpages连接",get_man_url,5);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!man","!man [0-9] <要查询的内容> 功能:返回linuxmanpages连接",get_man_url,5);
 
 		if(strstr(data,"!ip"))
-			parse_arg("PRIVMSG .[^ ]* :!ip","!ip <ipv4/6地址或域名> 功能:返回ipv4/6或者域名的物理位置",get_ip_addr,5);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!ip","!ip <ipv4/6地址或域名> 功能:返回ipv4/6或者域名的物理位置",get_ip_addr,5);
 
 		if(strstr(data,"!time"))
 		{
@@ -224,73 +224,73 @@ int main(int argc,char **argv)
 		}
 
 		if(strstr(data,"!dict"))
-			parse_arg("PRIVMSG .[^ ]* :!dict","!dict <目标语言> <要查询的内容> 功能:bing翻译",bing_dict,3);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!dict","!dict <目标语言> <要查询的内容> 功能:bing翻译",bing_dict,3);
 
 		if(strstr(data,"!youku"))
-			parse_arg("PRIVMSG .[^ ]* :!youku","!youku <关键词> 功能:返回youku第一个链接",get_youku_url,4);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!youku","!youku <关键词> 功能:返回youku第一个链接",get_youku_url,4);
 
 		if(strstr(data,"!bt"))
-			parse_arg("PRIVMSG .[^ ]* :!bt","!bt <关键词> 功能:返回btdigg.org第一个磁力链接",get_bt,4);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!bt","!bt <关键词> 功能:返回btdigg.org第一个磁力链接",get_bt,4);
 
 		if(strstr(data,"!zip"))
-			parse_arg("PRIVMSG .[^ ]* :!zip","!zip <地名> 功能:查询邮政编码",get_zip_code,4);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!zip","!zip <地名> 功能:查询邮政编码",get_zip_code,4);
 
 		if(strstr(data,"!weather"))
-			parse_arg("PRIVMSG .[^ ]* :!weather","!weather <地名> 功能:天气预报查询",get_weather,3);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!weather","!weather <地名> 功能:天气预报查询",get_weather,3);
 
 		if(strstr(data,"!stack"))
-			parse_arg("PRIVMSG .[^ ]* :!stack","!stack <问题> 功能:返回stackoverflow第一个搜索",get_stack,3);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!stack","!stack <问题> 功能:返回stackoverflow第一个搜索",get_stack,3);
 
 		if(strstr(data,"!id"))
-			parse_arg("PRIVMSG .[^ ]* :!id","!id <身份证号码> 功能:身份证号码查询",get_id_information,5);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!id","!id <身份证号码> 功能:身份证号码查询",get_id_information,5);
 
 		if(strstr(data,"!checkid"))
-			parse_arg("PRIVMSG .[^ ]* :!checkid","!checkid <身份证号码> 功能:校验身份证最后一位",check_id_card,5);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!checkid","!checkid <身份证号码> 功能:校验身份证最后一位",check_id_card,5);
 
 		if(strstr(data,"!url"))
-			parse_arg("PRIVMSG .[^ ]* :!url","!url <信息> 功能:url编码",get_url_encode,5);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!url","!url <信息> 功能:url编码",get_url_encode,5);
 
 		if(strstr(data,"!deurl"))
-			parse_arg("PRIVMSG .[^ ]* :!deurl","!deurl <url编码> 功能:url解码",get_url_decode,5);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!deurl","!deurl <url编码> 功能:url解码",get_url_decode,5);
 
 		if(strstr(data,"!joke"))
-			parse_arg("PRIVMSG .[^ ]* :!joke","!joke <关键词> 功能:返回一个小笑话",get_joke,2);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!joke","!joke <关键词> 功能:返回一个小笑话",get_joke,2);
 
 		if(strstr(data,"!dream"))
-			parse_arg("PRIVMSG .[^ ]* :!dream","!dream <关键词> 功能:周公解梦",get_dream,2);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!dream","!dream <关键词> 功能:周公解梦",get_dream,2);
 
 		if(strstr(data,"!song"))
-			parse_arg("PRIVMSG .[^ ]* :!song","!song <歌曲> 功能:返回百度音乐第一个搜索结果",get_song_url,2);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!song","!song <歌曲> 功能:返回百度音乐第一个搜索结果",get_song_url,2);
 
 		if(strstr(data,"!bing"))
-			parse_arg("PRIVMSG .[^ ]* :!bing","!bing <关键词> 功能:返回bing搜索第一个结果",get_bing,1);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!bing","!bing <关键词> 功能:返回bing搜索第一个结果",get_bing,1);
 
 		if(strstr(data,"!image"))
-			parse_arg("PRIVMSG .[^ ]* :!image","!image <关键词> 功能:返回google搜索第一张图片链接",get_google_image_url,1);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!image","!image <关键词> 功能:返回google搜索第一张图片链接",get_google_image_url,1);
 
 		if(strstr(data,"!google"))
-			parse_arg("PRIVMSG .[^ ]* :!google","!google <关键词> 功能:返回google搜索第一个结果",get_google,1);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!google","!google <关键词> 功能:返回google搜索第一个结果",get_google,1);
 
 		if(strstr(data,"!baidu"))
-			parse_arg("PRIVMSG .[^ ]* :!baidu","!baidu <关键词> 功能:返回baidu搜索第一个结果",get_baidu,1);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!baidu","!baidu <关键词> 功能:返回baidu搜索第一个结果",get_baidu,1);
 
 		if(strstr(data,"!bimg"))
-			parse_arg("PRIVMSG .[^ ]* :!bimg","!bimg <关键词> 功能:返回百度图片搜索第一个结果",get_bimg,1);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!bimg","!bimg <关键词> 功能:返回百度图片搜索第一个结果",get_bimg,1);
 
 		if(strstr(data,"!air"))
-			parse_arg("PRIVMSG .[^ ]* :!air","!air <城市名称> 功能:查询城市空气质量，城市名称使用拼音",get_air,1);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!air","!air <城市名称> 功能:查询城市空气质量，城市名称使用拼音",get_air,1);
 
 		if(strstr(data,"!website"))
-			parse_arg("PRIVMSG .[^ ]* :!website","!website <域名> 功能:网站安全检测，检测结果由360提供",get_website_testing,1);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!website","!website <域名> 功能:网站安全检测，检测结果由360提供",get_website_testing,1);
 
 		if(strstr(data,"!wifi"))
-			parse_arg("PRIVMSG .[^ ]* :!wifi","!wifi <城市名称> 功能:根据城市名返回查到的第一个wifi热点",get_wifi,1);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!wifi","!wifi <城市名称> 功能:根据城市名返回查到的第一个wifi热点",get_wifi,1);
 
 		if(strstr(data,"!train"))
-			parse_arg("PRIVMSG .[^ ]* :!train","!train <车次> 功能:火车时刻表查询",get_train,1);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!train","!train <车次> 功能:火车时刻表查询",get_train,1);
 
 		if(strstr(data,"!sm"))
-			parse_arg("PRIVMSG .[^ ]* :!sm","!sm <你想要说的话> 功能:与机器人对话，使用\'问题%答案\'方式可调教机器人",get_sm_message,1);
+			parse_arg("^:.[^ ]* PRIVMSG .[^ ]* :!sm","!sm <你想要说的话> 功能:与机器人对话，使用\'问题%答案\'方式可调教机器人",get_sm_message,1);
 
 		if(strstr(data,"!list"))
 		{
