8df917b918a7841781c109c85e3749dfdd6c91b1
brisk <briskgreen@163.com>
2014-07-23 23:45:55 +0800

    解码后的长度计算出错

--- base64.h
+++ base64.h
@@ -6,6 +6,6 @@
 
 unsigned char *base64_encode(unsigned char *str,long str_len);
 
-unsigned char *bae64_decode(unsigned char *code,long str_len);
+unsigned char *bae64_decode(unsigned char *code,long len);
 
 #endif
